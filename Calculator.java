6573b8567dcf745b83df3c9bfc8f4b54f6ede5fa
CakeSlayer <CakeSlayer@users.noreply.github.com>
2015-09-15 21:14:37 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,29 @@
 import java.lang.Math
 public class Calculator
 {
+  public static void main(String[] args)
+  {
+    double firstNumber;
+    double secondNumber;
+    double sum;
+    double difference;
+    double average;
+    double product;
+    
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter a number >> ");
+    firstNumber = input.nextDouble();
+    System.out.print("Please enter another number >> ");
+    secondNumber = input.nextDouble();
+    
+    sum = firstNumber + secondNumber;
+    difference = firstNumber - secondNumber;
+    average = sum/2;
+    product = firstNumber*secondNumber;
+    
+    System.out.println(firstNumber + " + " + secondNumber + " is " + sum);
+    System.out.println(firstNumber + " - " + secondNumber + " is " + difference);
+    System.out.println(firstNumber + " * "+ secondNumber + " is " + product);
+    System.out.println("The average of " + firstNumber + " and " + secondNumber + " is " + average);
+  }
 }
